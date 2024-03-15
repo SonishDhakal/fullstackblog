59b72bc22326f2821de5a379882b8fef423696df
Sonish <sonishdhakal122@gmail.com>
2024-03-15 15:36:15 +0545

    for depolyment

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import profileRoute from './routes/profile.route.js'
 import postRoute from './routes/post.route.js'
 import commentRoute  from './routes/commet.route.js'
 import userRoute  from './routes/user.route.js'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 const app = express();
 
@@ -40,6 +43,10 @@ app.use('/api/profile', profileRoute)
 app.use('/api/post', postRoute)
 app.use('/api/comment', commentRoute)
 app.use('/api/user', userRoute)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html')))
 
 
 
